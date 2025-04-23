549353b4a400913630e27ed47033669c2801eedd
Chetanyarajsingh <153371833+Chetanyarajsingh@users.noreply.github.com>
2025-04-23 18:58:43 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+//alert(8+2) we are using nodejs not browser
+
+console.log(8 + 2)
+console.log("chetanya")
+
+//number
+//bigInt
+//string
+//boolean
+//null
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
