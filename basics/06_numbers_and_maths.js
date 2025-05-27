7c7f5101a3074da424c7b9bc06cf8624734b016d
Chetanyarajsingh <153371833+Chetanyarajsingh@users.noreply.github.com>
2025-05-27 20:38:38 +0000

    numbers and maths done

---
+++ basics/06_numbers_and_maths.js
@@ -0,0 +1,31 @@
+const score = 400
+// console.log(score);
+// or
+const value = new Number(100)
+// console.log(value);
+
+// console.log(value.toString().length);
+// console.log(value.toFixed(2));
+
+const otherNumber = 91.8973
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// -----------------------------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.4));
+// console.log(Math.min(5,4,6,8,1));
+// console.log(Math.max(5,4,6,8,1));
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min)
