ef013facc1686b77414f5c6fe8e7a2a2de4c3986
Chetanyarajsingh <153371833+Chetanyarajsingh@users.noreply.github.com>
2025-06-03 20:49:40 +0000

    functions started

---
+++ basics_3/01_functions.js
@@ -0,0 +1,12 @@
+function sayMyname(){
+    console.log("C");
+    console.log("H");
+    console.log("E");
+    console.log("T");
+    console.log("A");
+    console.log("N");
+    console.log("Y");
+    console.log("A");
+}
+
+sayMyname()
